2f3e18d0f913877be35a65bd8ed278969a07126f
senbeiman <1000bei10000@gmail.com>
2019-10-07 18:13:10 +0900

    bug fix

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, jsonify
+import json
 
 
 app = Flask(__name__)
-color = '#ffffff'
+with open('color.json') as f:
+    color = json.load(f)["color"]
 
 
 @app.route('/', methods=["GET"])
@@ -12,15 +14,17 @@ def render_html():
 
 @app.route('/color', methods=["POST"])
 def change_color():
-    global color
     color = request.data.decode('utf-8')
     print("post", color)
+    with open('color.json', 'w') as f:
+        json.dump({"color": color}, f)
     return "ok"
 
 
 @app.route('/color', methods=["GET"])
 def get_color():
-    global color
+    with open('color.json') as f:
+        color = json.load(f)["color"]
     print('get', color)
     return color
     
