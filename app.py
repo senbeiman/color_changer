8a6346a2466f373ac71040996c033a10895e9795
senbeiman <1000bei10000@gmail.com>
2019-10-07 17:24:02 +0900

    first deployment

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+
+
+app = Flask(__name__)
+color = '#ffffff'
+
+
+@app.route('/', methods=["GET"])
+def render_html():
+    return render_template("index.html")
+
+
+@app.route('/', methods=["POST"])
+def change_color():
+    global color
+    color = request.form["color"]
+    return render_template("index.html")
+
+
+@app.route('/color', methods=["GET"])
+def get_color():
+    return color
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True)
